c4356b5e36c1988e619e0d464d110a5227814bd3
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2019-06-20 14:49:43 +0300

    nesting components

---
+++ src/components/Ninjas.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Ninjas extends Component {
+
+    
+    render() {
+        return (
+            <div className="ninja">
+                <div>Name: Ryu</div>
+                <div>Age: 12</div>
+                <div>Belt: black</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas
+
